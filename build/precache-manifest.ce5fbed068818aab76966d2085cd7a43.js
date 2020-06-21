c60e3252dd6b72d9f2143b8446136e73c69f5ad4
Sriraman <sriramancse@gmail.com>
2020-06-21 21:47:52 +0530

    Build included

---
+++ build/precache-manifest.ce5fbed068818aab76966d2085cd7a43.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5c321a7eae1161ae0f1ed5105f96ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ce9f769f99d8cf14492",
+    "url": "/static/css/main.d663e03d.chunk.css"
+  },
+  {
+    "revision": "91d90906bc81850f8fcc",
+    "url": "/static/js/2.ea6fac1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ea6fac1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce9f769f99d8cf14492",
+    "url": "/static/js/main.99d3ff07.chunk.js"
+  },
+  {
+    "revision": "791e36abef7809e1abff",
+    "url": "/static/js/runtime-main.0633b913.js"
+  },
+  {
+    "revision": "b9a8b6c12f2c4cfe9a3e4701af679cd4",
+    "url": "/static/media/Preethi.b9a8b6c1.png"
+  },
+  {
+    "revision": "4b2c5ac01060f201ae248de0935b2513",
+    "url": "/static/media/Sri.4b2c5ac0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "a96fc65e8927cfb7bf57adf99fa76b21",
+    "url": "/static/media/sriandpreethi.a96fc65e.png"
+  }
+]);
